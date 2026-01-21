03577b0660bae2fa6688bcf4d5f5012b6717d689
Abhishek <abhisheks@observancegroup.com>
2026-01-21 17:42:23 +0530

    SEO improved

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,9 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col bg-zinc-50 font-sans dark:bg-black">
-      
-     
-
       <main className="flex flex-1 flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black">
         <p className="text-xl font-semibold">Welcome to Pathoconnect</p>
       </main>
-
-    
     </div>
   );
 }

3d1b9741e3cc72c89e591076bd628ab37898b083
Abhishek <abhisheks@observancegroup.com>
2026-01-21 16:20:11 +0530

    Footer created, Contact us page created

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -1,17 +1,18 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 
-export default function Home() {
+export default function ContactUs() {
   return (
     <div className="flex min-h-screen flex-col bg-zinc-50 font-sans dark:bg-black">
       
      
 
       <main className="flex flex-1 flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black">
-        <p className="text-xl font-semibold">Welcome to Pathoconnect</p>
+        <p className="text-xl font-semibold">Welcome to Pathoconnect Contact us page</p>
       </main>
 
-    
+  
+
     </div>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -4,63 +4,94 @@ import Image from "next/image";
 import Link from "next/link";
 import PathoLogo from "@/assets/images/PathoLogo.png";
 import Phone from "@/assets/images/Phone.png";
+import { log } from "console";
+
+export function CustomItems({
+  title,
+  items = [],
+  route,
+}: {
+  title: string;
+  items?: { label: string; href: string }[];
+  route: string;
+}) {
+  return (
+    <div className="relative group cursor-pointer h-full bg-white items-center flex">
+      {route ? (
+        <Link href={route} className="hover:text-blue-600">
+          {title}
+        </Link>
+      ) : (
+        <span className="hover:text-blue-600">{title}</span>
+      )}
+
+      <div className="absolute left-0 top-full mt-2 invisible opacity-0 group-hover:visible group-hover:opacity-100 bg-white shadow-lg rounded-md min-w-[180px] transition-all duration-200">
+        {items &&
+          items?.map((item, index) => (
+            <Link
+              key={index}
+              href={item.href}
+              className="block px-4 py-2 hover:bg-gray-100 whitespace-nowrap"
+            >
+              {item.label}
+            </Link>
+          ))}
+      </div>
+    </div>
+  );
+}
+
+const productItems = [
+  { label: "App", href: "/product/app" },
+  { label: "Web", href: "/product/web" },
+  { label: "AI", href: "/product/ai" },
+];
+
+const aboutItems = [
+  { label: "Company", href: "/about/company" },
+  { label: "Team", href: "/about/team" },
+  { label: "Careers", href: "/about/career" },
+];
+
+const blogItems = [
+  {
+    label:
+      "Phlebotomy App and Sample Collection Software for Clinical Pathology",
+    href: "/blog/tech",
+  },
+  {
+    label: "Transform Lab Management with Our Advanced CRM Dashboard",
+    href: "/blog/business",
+  },
+  { label: "Online Lab Test Booking Application", href: "/blog/design" },
+];
+const integrationItems = [
+  { label: "Software Integration", href: "/blog/tech" },
+  { label: "Device Integration", href: "/blog/business" },
+];
 
 export default function Header() {
   return (
-    <header className="fixed top-0 left-0 w-full bg-white shadow z-50">
-      <div className="max-w-7xl mx-auto flex items-center justify-between px-6 py-3 ">
-
+    <header className="fixed top-0 left-0 w-full h-20 bg-white shadow z-50">
+      <div className="max-w-7xl mx-auto h-full flex items-center justify-between px-6 py-0 ">
         {/* Logo */}
         <Link href="/">
           <Image src={PathoLogo} alt="Logo" width={180} height={50} />
         </Link>
 
         {/* Menu */}
-        <nav className="flex gap-8 text-gray-800 font-medium">
-
+        <nav className="flex gap-8 text-gray-800 h-full items-center font-medium">
           {/* Home */}
-          <Link href="/" className="hover:text-blue-600">
-            Home
-          </Link>
-
-          {/* Product Dropdown */}
-          <div className="relative group cursor-pointer">
-            <span className="hover:text-blue-600">Product</span>
-
-            <div className="absolute left-0 top-full mt-2 hidden group-hover:block bg-white shadow-lg rounded-md min-w-[180px]">
-              <Link href="/product/app" className="block px-4 py-2 hover:bg-gray-100">App</Link>
-              <Link href="/product/web" className="block px-4 py-2 hover:bg-gray-100">Web</Link>
-              <Link href="/product/ai" className="block px-4 py-2 hover:bg-gray-100">AI</Link>
-            </div>
-          </div>
-
-          {/* About Dropdown */}
-          <div className="relative group cursor-pointer">
-            <span className="hover:text-blue-600">About Us</span>
-
-            <div className="absolute left-0 top-full mt-2 hidden group-hover:block bg-white shadow-lg rounded-md min-w-[180px]">
-              <Link href="/about/company" className="block px-4 py-2 hover:bg-gray-100">Company</Link>
-              <Link href="/about/team" className="block px-4 py-2 hover:bg-gray-100">Team</Link>
-              <Link href="/about/career" className="block px-4 py-2 hover:bg-gray-100">Careers</Link>
-            </div>
-          </div>
-
-          {/* Blog Dropdown */}
-          <div className="relative group cursor-pointer">
-            <span className="hover:text-blue-600">Blog</span>
-
-            <div className="absolute left-0 top-full mt-2 hidden group-hover:block bg-white shadow-lg rounded-md min-w-[180px]">
-              <Link href="/blog/tech" className="block px-4 py-2 hover:bg-gray-100">Tech</Link>
-              <Link href="/blog/business" className="block px-4 py-2 hover:bg-gray-100">Business</Link>
-              <Link href="/blog/design" className="block px-4 py-2 hover:bg-gray-100">Design</Link>
-            </div>
-          </div>
-
+          <CustomItems title="Home" route="/" />
+          <CustomItems title="Product" items={productItems} />
+          <CustomItems title="About Us" />
+          <CustomItems title="Blog" items={blogItems} />
+          <CustomItems title="Integration" items={integrationItems} />
+          <CustomItems title="Contact Us" route="/contact-us" />
         </nav>
-         <Link href="/">
+        <Link href="/">
           <Image src={Phone} alt="Logo" width={40} height={40} />
         </Link>
-
       </div>
     </header>
   );

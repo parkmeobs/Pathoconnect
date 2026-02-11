3359a56c0493a3d65f7e097ea05b0c96fcfd9ca0
Abhishek <abhisheks@observancegroup.com>
2026-02-11 18:09:10 +0530

    Blog partially completed,colors defined,tail

---
+++ src/app/integration/[slug]/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default async function IntegrationPage({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+})  {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
